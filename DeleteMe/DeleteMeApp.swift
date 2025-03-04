963e049fb9b33b403e677a34309771e9c57001a0
dljardim <dljardim@outlook.com>
2025-03-04 14:39:15 -0600

    Initial Commit

---
+++ DeleteMe/DeleteMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeleteMeApp.swift
+//  DeleteMe
+//
+//  Created by Damian Jardim on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct DeleteMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
